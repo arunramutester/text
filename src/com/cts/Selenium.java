4b9bd52f2354cce65ac3db7c2e1a9a6b3a81f8d8
arunramutester <arunramu092@gmail.com>
2022-01-07 12:56:12 +0530

    push

---
+++ src/com/cts/Selenium.java
@@ -0,0 +1,23 @@
+package com.cts;
+
+public class Selenium {
+	
+	public void honda() {
+		
+		System.out.println("cars");
+	}
+	
+	public void toyota() {
+		
+		System.out.println("inova");
+	}
+	public static void main(String[] args) {
+		
+		Selenium s = new Selenium();
+		
+		s.honda();
+		
+		s.toyota();
+	}
+
+}
